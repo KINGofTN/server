9d738426625a80b4f293a0eba08efe6945ddc395
Tamilan... <142815476+KINGofTN@users.noreply.github.com>
2024-12-26 23:09:18 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import socket
+import os
+
 
 # Server configuration
 HOST = '0.0.0.0'  # Listen on all available interfaces
 PORT = 5000       # Port to listen on
-
+PORT = int(os.getenv('PORT', 5000))
 def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, PORT))
